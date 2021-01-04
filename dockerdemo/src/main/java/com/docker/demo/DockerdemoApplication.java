dd10245777c2bbac4fa0856e03599c61c88ad4cb
RajeevBannu <rajeev.centurylink@gmail.com>
2021-01-04 12:25:51 -0600

    First Commit to git

---
+++ dockerdemo/src/main/java/com/docker/demo/DockerdemoApplication.java
@@ -0,0 +1,20 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerdemoApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "<h1> hello spring boot and k8 </h1>";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemoApplication.class, args);
+	}
+
+}
